4037b52190fba17fd2afde31164421683f5fa699
MRodr4539 <miranda.rodriguez4539@yahoo.com>
2019-12-04 15:19:10 -0600

    working on delete

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import {Button, Table, TableHead, TableBody, TableRow, TableCell, Container } from "@material-ui/core"
+import { MoreVert } from '@material-ui/icons'
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <div>
+      <p>Import Component</p>
+        <Button variant='contained' color='primary' onClick={props.fetchMakes}>
+           Import
+        </Button>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {props.makes.map(row => (
+                    <TableRow key={row.name}>
+                        <TableCell>{row['MakeId']}</TableCell>
+                        <TableCell>{row['MakeName']}</TableCell>
+                        <TableCell>
+                          <DeleteIcon onClick={() => props.deleteMakes(index)}>
+                          </DeleteIcon>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+        
+        </div>
+      
     )
 }
 
